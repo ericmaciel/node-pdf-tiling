44bf01a9db6dadec2950151c3e0b8fe48f93432c
Bruno Santos <brunonascimentosantos@gmail.com>
2015-01-21 17:21:01 -0200

    converting pdf into pictures each page one png

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express'),
-	multer  = require('multer')
+	multer  = require('multer'),
+	fs = require('fs'),
+	Canvas = require('canvas'),
+	PDFJS = require('./lib/pdf.js'),
+	PDFReader = require('./lib/reader.js').PDFReader
+
+PDFJS.disableWorker = true
 
 var app = express()
 app.use(
@@ -17,15 +23,34 @@ app.get('/', function (req, res) {
 })
 
 app.post('/upload',function(req,res){
-	console.log(req.files)
-	res.end('File uploaded')
+	var fileName = req.files.file.name
+	var destFolder = __dirname + '/uploads/' + fileName.substring(0, fileName.indexOf('.pdf'))
+	fs.mkdirSync(destFolder)
+
+	var pdf = new PDFReader(__dirname + '/' + req.files.file.path)
+	pdf.on('error', errorDumper);
+	pdf.on('ready', function(pdf) {
+		// Render all pages.
+		pdf.renderAll({
+
+			bg: true,
+			output: function(pageNum) {
+				return destFolder + '/page' + pageNum + '.png'
+			}
+			}, errorDumper)
+		res.end('File uploaded and converted')
+	});
 });
 
-var server = app.listen(3000, function () {
+function errorDumper(err) {
+  if (err) {
+    console.log('something went wrong :/')
+    throw err
+  }
+}
 
+var server = app.listen(3000, function () {
   var host = server.address().address
   var port = server.address().port
-
   console.log('Example app listening at http://%s:%s', host, port)
-
 })
